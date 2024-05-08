11e1115ef58818829a684b6672b8f5be1a4dfe62
OSABOSA <oskar.siemieniuk01@gmail.com>
2024-05-08 10:48:03 +0200

    adding files and tests

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 # przykladowe funkcje utils .py
+
+# with this one:
 def add(a: int, b: int) -> int:
     return a + b
 
-
+# przykladowe funkcje utils .py
 def subtract(a: int, b: int) -> int:
     return a - b
 
 
+# przykladowe funkcje utils .py
 def multiply(a: int, b: int) -> int:
     return a * b
 
 
+# przykladowe funkcje utils .py
 def divide(a: int, b: int) -> float:
     return a / b
