169866f0cf3b8a576a288760704a97e5695a17b2
OSABOSA <oskar.siemieniuk01@gmail.com>
2024-05-08 10:50:06 +0200

    adding files and tests

--- utils.py
+++ utils.py
@@ -1,19 +1,56 @@
-# przykladowe funkcje utils .py
+'''# przykladowe funkcje utils.py'''
 
-# with this one:
 def add(a: int, b: int) -> int:
+    """
+    Adds two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of the two integers.
+    """
     return a + b
 
-# przykladowe funkcje utils .py
+
 def subtract(a: int, b: int) -> int:
+    """
+    Subtracts one integer from another.
+
+    Args:
+        a (int): The integer to be subtracted from.
+        b (int): The integer to subtract.
+
+    Returns:
+        int: The result of the subtraction.
+    """
     return a - b
 
 
-# przykladowe funkcje utils .py
 def multiply(a: int, b: int) -> int:
+    """
+    Multiplies two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The product of the two integers.
+    """
     return a * b
 
 
-# przykladowe funkcje utils .py
 def divide(a: int, b: int) -> float:
+    """
+    Divides one integer by another.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        float: The result of the division.
+    """
     return a / b
